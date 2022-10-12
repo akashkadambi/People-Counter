d40924eed69ee826e05ce3db2cfd4eca1b6257dc
akashkadambi <78289577+akashkadambi@users.noreply.github.com>
2022-10-12 12:24:07 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,4 +1,4 @@
-account_sid='AC10927dbfcf79cc96ed2abcc206ff65e3'
-auth_token='81691dbf1c7c0df451474e94d6200bcd'
-twilio_number='+12184383885'
-target_number='+919493626256'
+account_sid='GET_FROM_TWILIO_ACCOUNT'
+auth_token='GET_FROM_TWILIO_ACCOUNT'
+twilio_number='YOUR_TWILIO_NUMBER'
+target_number='YOUR_MOBILE_NUMBER'
